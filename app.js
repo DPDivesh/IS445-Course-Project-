3debf187546824a10d7a24b37deacd09021353df
Divesh <diveshpatel39@yahoo.com>
2020-05-05 19:30:49 -0700

    Adding firebase config and starting html setup and bootstrap

---
+++ app.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB1ZbKz_5jUtKaJtga8QzwmS0geABoqIeA",
+    authDomain: "is445-course-project-2d084.firebaseapp.com",
+    databaseURL: "https://is445-course-project-2d084.firebaseio.com",
+    projectId: "is445-course-project-2d084",
+    storageBucket: "is445-course-project-2d084.appspot.com",
+    messagingSenderId: "123316226579",
+    appId: "1:123316226579:web:10940c95a97bc347b98de6",
+    measurementId: "G-KH22XVDB7M"
+  };
